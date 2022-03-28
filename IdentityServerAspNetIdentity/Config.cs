e443f8e6dbb9761df0bbd87deacc3c397dc6dc44
Bao Nguyen <thanhbao0408@gmail.com>
2022-03-28 22:45:43 +0700

    - Saved

--- IdentityServerAspNetIdentity/Config.cs
+++ IdentityServerAspNetIdentity/Config.cs
@@ -14,39 +14,27 @@ public static class Config
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
         {
-            new ApiScope("scope1"),
-            new ApiScope("scope2"),
+            new ApiScope("SampleAPI"),
         };
 
     public static IEnumerable<Client> Clients =>
         new Client[]
         {
-            // m2m client credentials flow client
-            new Client
-            {
-                ClientId = "m2m.client",
-                ClientName = "Client Credentials Client",
+             // Swagger client
+                new Client
+                {
+                    ClientId = "api_swagger",
+                    ClientName = "Swagger UI for Sample API",
+                    ClientSecrets = {new Secret("secret".Sha256())}, // change me!
 
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
 
-                AllowedScopes = { "scope1" }
-            },
-
-            // interactive client using code flow + pkce
-            new Client
-            {
-                ClientId = "interactive",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                AllowedGrantTypes = GrantTypes.Code,
-
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                AllowOfflineAccess = true,
-                AllowedScopes = { "openid", "profile", "scope2" }
-            },
+                    RedirectUris = {"https://localhost:7101/swagger/oauth2-redirect.html"},
+                    AllowedCorsOrigins = {"https://localhost:7101"},
+                    AllowedScopes = new List<string>
+                    {
+                        "SampleAPI"
+                    }
+                },
         };
 }
--- WebApiDemo/Program.cs
+++ WebApiDemo/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,10 +8,58 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri("https://localhost:5001/connect/authorize"),
+                TokenUrl = new Uri("https://localhost:5001/connect/token"),
+                Scopes = new Dictionary<string, string>
+                {
+                    {"SampleAPI", "Sample API - full access"}
+                }
+            },
+        }
+    });
+
+    // Apply Scheme globally
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+            },
+            new[] { "SampleAPI" }
+        }
+    });
+});
 
 builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer("Bearer", options => { });
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:5001"; // Our API app will call to the IdentityServer to get the authority
+
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateAudience = false, // Validate 
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    // Add a policy called "ApiScope" which is required our users are authenticated and have the API Scope Claim call WebAPI 
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "WebAPI");
+    });
+});
 
 var app = builder.Build();
 
@@ -16,7 +67,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.OAuthUsePkce();
+    });
 }
 
 app.UseHttpsRedirection();
