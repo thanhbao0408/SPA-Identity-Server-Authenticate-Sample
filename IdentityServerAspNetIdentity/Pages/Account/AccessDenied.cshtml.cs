d6bdcbfdd2dd2b508438eb88efd4cd3965b9f73a
Bao Nguyen <thanhbao0408@gmail.com>
2022-03-28 08:39:30 +0700

    Save

---
+++ IdentityServerAspNetIdentity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityServerAspNetIdentity.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
